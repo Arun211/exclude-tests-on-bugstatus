7ade6e976d989c7bdbffad0e50522cb951d3c1f0
arunkumar.v <arunkumar.v@appviewx.com>
2018-01-24 22:08:15 +0530

    Redmine bug status check implementation

--- src/main/java/org/allselenium/redmineImpl/RedmineImpl.java
+++ src/main/java/org/allselenium/redmineImpl/RedmineImpl.java
@@ -2,25 +2,55 @@ package org.allselenium.redmineImpl;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Random;
+
+import com.taskadapter.redmineapi.IssueManager;
+import com.taskadapter.redmineapi.RedmineException;
+import com.taskadapter.redmineapi.RedmineManager;
+import com.taskadapter.redmineapi.RedmineManagerFactory;
+import com.taskadapter.redmineapi.bean.Issue;
 
 public class RedmineImpl {
-
-	private static List<String> bugStatus = Arrays.asList("New", "Assigned", "Inprogress", "Resolved", "Reopened", "Verified");
-	private static List<String> bugStatusToSkip = Arrays.asList("New", "Assigned", "Reopened", "Inprogress");
 	
+	/**
+	 * We may not need objects of this class for validation.
+	 */
+	private RedmineImpl() {};
+
 	public static boolean isBugValid(String bugId) {
-		if(bugId != null && bugStatusToSkip.contains(getBugStatus(bugId))) {
+		List<String> bugStatusToSkip = Arrays.asList("New", "Assigned", "Reopened", "Inprogress");
+		int id = 0;
+		
+		try {
+			id = Integer.parseInt(bugId);
+		} catch(NumberFormatException|NullPointerException exception) {
+			return false;
+		}
+		
+		if(bugStatusToSkip.contains(getBugStatus(id))) {
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	private static String getBugStatus(String bugId) {
-		/**
-		 * code to get bug status
-		 */
-		return bugStatus.get(new Random().nextInt(6));
+	private static String getBugStatus(int bugId) {
+	    String uri = "https://RedmineInstalledURL/";
+	    
+	    //API access key; Will be available in 'MyAccount' page in Redmine website
+	    String apiAccessKey = "3cf072b039a8d07045f986562b25aa55087630be";
+	
+	    RedmineManager mgr = RedmineManagerFactory.createWithApiKey(uri, apiAccessKey);
+	    IssueManager issueManager = mgr.getIssueManager();
+	
+	    //Get issue by ID
+	    Issue issue;
+		try {
+			issue = issueManager.getIssueById(bugId);
+		} catch (RedmineException e) {
+			return null;
+		}
+		
+		//Get issue status 
+	    return issue.getStatusName();
 	}
 }
--- src/main/java/org/allselenium/util/DisableTest.java
+++ src/main/java/org/allselenium/util/DisableTest.java
@@ -3,15 +3,18 @@ package org.allselenium.util;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
 
+import org.allselenium.jiraImpl.JiraImpl;
 import org.allselenium.redmineImpl.RedmineImpl;
 import org.testng.IAnnotationTransformer;
 import org.testng.annotations.ITestAnnotation;
 
 public class DisableTest implements IAnnotationTransformer {
-	public ReadProperty readProperty;
+	private ReadProperty readProperty;
+	private String bugManagementSystem;
 	
 	public DisableTest() {
 		readProperty = new ReadProperty(System.getProperty("user.dir")+"/TestcaseBugIdMapping.properties");
+		this.bugManagementSystem = readProperty.getValue("BugManagementSystem");
 	}
 	
 	@Override
@@ -25,8 +28,16 @@ public class DisableTest implements IAnnotationTransformer {
 		 *  	2. Test name attribute available for @Test annotation
 		 */
 		String bugId = readProperty.getValue(testMethod.getName());
-		if(RedmineImpl.isBugValid(bugId)) {
-			annotation.setEnabled(false);
+		if(bugManagementSystem.equalsIgnoreCase("Redmine")) {
+			if(RedmineImpl.isBugValid(bugId)) {
+				annotation.setEnabled(false);
+			}
+		} else if(bugManagementSystem.equalsIgnoreCase("Jira")){
+			if(JiraImpl.isBugValid(bugId)) {
+				annotation.setEnabled(false);
+			}
+		} else {
+			
 		}
 	}
 	
